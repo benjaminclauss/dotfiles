de9c4e647211464cd773a78d096519488e1dc269
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-06-08 17:55:11 -0500

    Install Steam
    I am so unproductive!!!

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,8 @@
 #!/bin/zsh
 echo "Symlinking ~/.zprofile..."
 ln -nfs /Users/$USER/Developer/dotfiles/.zprofile /Users/$USER/.zprofile
+
+
+echo "Symlinking ~/.Brewfile."
+ln -nfs /Users/$USER/Developer/dotfiles/Brewfile /Users/$USER/.Brewfile
 echo "Setup complete."
