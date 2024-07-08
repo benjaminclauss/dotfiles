d6bd03816bfd2d7c35c576925592602f827f7bff
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-07-07 19:15:24 -0500

    Install OpenJDK
    For the system Java wrappers to find this JDK, symlink it with
      sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
    
    openjdk is keg-only, which means it was not symlinked into /opt/homebrew,
    because macOS provides similar software and installing this software in
    parallel can cause all kinds of trouble.
    
    If you need to have openjdk first in your PATH, run:
      echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
    
    For compilers to find openjdk you may need to set:
      export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
    
    Skype and Kap are also included.

---
+++ .zshrc
@@ -0,0 +1,4 @@
+# If you need to have openjdk first in your PATH
+export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+# For compilers to find openjdk you may need to set:
+export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
--- setup.zsh
+++ setup.zsh
@@ -2,7 +2,9 @@
 echo "Symlinking ~/.zprofile..."
 ln -nfs /Users/$USER/Developer/dotfiles/.zprofile /Users/$USER/.zprofile
 
-
 echo "Symlinking ~/.Brewfile."
 ln -nfs /Users/$USER/Developer/dotfiles/Brewfile /Users/$USER/.Brewfile
 echo "Setup complete."
+
+# For the system Java wrappers to find this JDK, symlink it.
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
