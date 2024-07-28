b54ec45c44e9df202d957e696a306ca0958b9e9c
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-07-28 16:18:59 -0400

    Install VimGolf
    time to git gud

--- setup.zsh
+++ setup.zsh
@@ -6,5 +6,8 @@ echo "Symlinking ~/.Brewfile."
 ln -nfs /Users/$USER/Developer/dotfiles/Brewfile /Users/$USER/.Brewfile
 echo "Setup complete."
 
+# Install Ruby Gems.
+bundle install
+
 # For the system Java wrappers to find this JDK, symlink it.
 sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
