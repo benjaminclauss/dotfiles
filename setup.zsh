cd1c2a0ae4153f5f399ea4f6d96c2689d4cf0c92
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-11-24 15:11:26 +0100

    Put screesnshots in dedicated directory

--- setup.zsh
+++ setup.zsh
@@ -4,10 +4,15 @@ ln -nfs /Users/$USER/Developer/dotfiles/.zprofile /Users/$USER/.zprofile
 
 echo "Symlinking ~/.Brewfile."
 ln -nfs /Users/$USER/Developer/dotfiles/Brewfile /Users/$USER/.Brewfile
-echo "Setup complete."
 
 # Install Ruby Gems.
 bundle install
 
 # For the system Java wrappers to find this JDK, symlink it.
 sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+# Create screenshot directory.
+mkdir -p /Users/$USER/Pictures/Screenshots
+# https://www.macworld.com/article/673251/how-to-change-where-screenshots-are-saved-on-a-mac.html
+
+echo "Setup complete."
